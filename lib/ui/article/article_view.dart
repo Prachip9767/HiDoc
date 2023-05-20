659c7daac9d5105494f543cb1f9382b86dc410cb
prachip293@gmail.com <prachip293@gmail.com>
2023-05-20 20:50:54 +0530

    ui fix

--- lib/ui/article/article_view.dart
+++ lib/ui/article/article_view.dart
@@ -15,149 +15,185 @@ class ArticleView extends GetView<ArticleController> {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      if (controller.isLoading.value){
+      if (controller.isLoading.value) {
         return const Scaffold(
           body: Center(
             child: CircularProgressIndicator(),
           ),
         );
-      }else{
+      } else {
         return Scaffold(
-          backgroundColor: Colors.deepOrange[50],
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 14, bottom: 14),
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height:40,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(18),
-                        bottomRight: Radius.circular(18)),
-                    color: Colors.lightBlueAccent.withOpacity(0.4),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'hiDoc',
-                      style: context.textTheme.bodyLarge?.copyWith(
-                        fontWeight: FontWeight.w800,
-                        color: Colors.white,
-                        fontSize: 18,
-                        letterSpacing: 1,
+          backgroundColor: Colors.grey[200],
+          body: Center(
+            child: Container(
+              width: kIsWeb ? 900 : double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 14, bottom: 14),
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(18),
+                            bottomRight: Radius.circular(18)),
+                        color: Colors.lightBlueAccent.withOpacity(0.4),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'hiDoc',
+                          style: context.textTheme.bodyLarge?.copyWith(
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                            fontSize: 18,
+                            letterSpacing: 1,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'ARTICLES',
-                    style: context.textTheme.bodyLarge?.copyWith(
-                      fontWeight: FontWeight.w800,
-                      color: Colors.black,
-                      fontSize: 18,
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'ARTICLES',
+                        style: context.textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal:16),
+                    child: Container(
+                      width: 600,
+                      height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Spacer(),
+                            SizedBox(width: 50,),
+                            Text(
+                              'Criticle Care',
+                              style: context.textTheme.labelLarge,
+                            ),
+                            Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              child: Icon(Icons.arrow_drop_down_outlined),
+                            ),
+                          ],
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          TopCardWidget(
+                            articleDescription: controller
+                                    .singleEvent.value?.article.articleImg ??
+                                '',
+                            articleTitle: controller
+                                    .singleEvent.value?.article.articleTitle ??
+                                'No Title',
+                            imageUrl: controller.singleEvent.value?.article
+                                    .articleDescription ??
+                                'No Description',
+                          ),
+                          if (kIsWeb) ...[
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: HidocBulletin(
+                                    articleModel: controller
+                                        .singleEvent.value!.trandingBulletin,
+                                  ),
+                                ),
+                                const SizedBox(width: 16),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 16),
+                                    child: TrendingBulletin(
+                                      article: controller
+                                          .singleEvent.value!.trandingBulletin,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ] else ...[
+                            HidocBulletin(
+                              articleModel:
+                                  controller.singleEvent.value!.trandingBulletin,
+                            ),
+                            const SizedBox(height: 16),
+                            TrendingBulletin(
+                              article:
+                                  controller.singleEvent.value!.trandingBulletin,
+                            ),
+                          ],
+                          if (kIsWeb) ...[
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: ArticleWidget(
+                                    title: 'Trending Article',
+                                    article: controller
+                                        .singleEvent.value!.trandingArticle,
+                                  ),
+                                ),
+                                const SizedBox(width: 16),
+                                Expanded(
+                                  child: ArticleWidget(
+                                    article: controller
+                                        .singleEvent.value!.exploreArticle,
+                                    title: 'Explore More Article',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ] else ...[
+                            ArticleWidget(
+                              title: 'Trending Article',
+                              article:
+                                  controller.singleEvent.value!.trandingArticle,
+                            ),
+                            ArticleWidget(
+                              article:
+                                  controller.singleEvent.value!.exploreArticle,
+                              title: 'Explore More Article',
+                            ),
+                          ],
+                          NewsWidget(
+                            news: controller.singleEvent.value!.news,
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 16,
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      TopCardWidget(
-                        articleDescription:
-                        controller.singleEvent.value?.article.articleImg ??
-                            '',
-                        articleTitle:
-                        controller.singleEvent.value?.article.articleTitle ??
-                            'No Title',
-                        imageUrl: controller
-                            .singleEvent.value?.article.articleDescription ??
-                            'No Description',
-                      ),
-                      if (kIsWeb) ...[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: HidocBulletin(
-                                articleModel: controller
-                                    .singleEvent.value!.trandingBulletin,
-                              ),
-                            ),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 16),
-                                child: TrendingBulletin(
-                                  article: controller
-                                      .singleEvent.value!.trandingBulletin,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ] else ...[
-                        HidocBulletin(
-                          articleModel:
-                          controller.singleEvent.value!.trandingBulletin,
-                        ),
-                        const SizedBox(height: 16),
-                        TrendingBulletin(
-                          article: controller.singleEvent.value!.trandingBulletin,
-                        ),
-                      ],
-                      if (kIsWeb) ...[
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: ArticleWidget(
-                                title: 'Trending Article',
-                                article:
-                                controller.singleEvent.value!.trandingArticle,
-                              ),
-                            ),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: ArticleWidget(
-                                article:
-                                controller.singleEvent.value!.exploreArticle,
-                                title: 'Explore More Article',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ] else ...[
-                        ArticleWidget(
-                          title: 'Trending Article',
-                          article: controller.singleEvent.value!.trandingArticle,
-                        ),
-                        ArticleWidget(
-                          article: controller.singleEvent.value!.exploreArticle,
-                          title: 'Explore More Article',
-                        ),
-                      ],
-                      NewsWidget(news: controller.singleEvent.value!.news,)
-                    ],
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         );
-
       }
     });
   }
